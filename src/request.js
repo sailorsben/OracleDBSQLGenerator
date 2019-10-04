35c0fbb340aabef4072b36d61198440a4aec05bb
bsailors <bsailors@newgistics.com>
2019-10-04 16:15:55 -0500

    Cleaning up some garbage

--- src/request.js
+++ src/request.js
@@ -3,9 +3,9 @@ import { COLUMNS } from './columns.js'
 
 var request = {
   desiredFields: [COLUMNS.DIM_WEIGHT, COLUMNS.LENGTH, COLUMNS.HEIGHT, COLUMNS.WIDTH],
-  merchantName: "Yan Wen",
-  merchantID: 2867,
-  ticketNumber: 16101,
+  merchantName: "Office Depot",
+  merchantID: 2769,
+  ticketNumber: 15059,
   saveLocation: 'C:\\Users\\be016sa\\Documents\\ngs_bill_database_oracle_newg\\scripts'
 }
 
